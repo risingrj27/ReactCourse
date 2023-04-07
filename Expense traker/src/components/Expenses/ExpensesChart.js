44ad5c301f27f619ae832eeb08114faf549f73e9
Ritesh jangra <rjprince18@gmail.com>
2023-04-07 09:38:11 +0530

    7-4-2023

--- Expense traker/src/components/Expenses/ExpensesChart.js
+++ Expense traker/src/components/Expenses/ExpensesChart.js
@@ -18,6 +18,7 @@ const ExpensesChart = (props) => {
   ];
 
   for(const expense of props.expenses){
+    
     const expenseMonth = expense.date.getMonth(); //starting at 0 => january
     chartDataPoints[expenseMonth].value += expense.amount;
   }
