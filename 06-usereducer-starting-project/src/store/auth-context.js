cc3960cde1b562ee3ee8eecfe61b67e20c2c190c
Ritesh jangra <rjprince18@gmail.com>
2023-04-12 23:46:31 +0530

    msi

--- 06-usereducer-starting-project/src/store/auth-context.js
+++ 06-usereducer-starting-project/src/store/auth-context.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const AuthContext = React.createContext({
-    isLoggedIn : false
+    isLoggedIn : false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 })
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState()
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    
+        if (storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true);
+        }
+      }, []);
+    
+
+    const loginHandler= () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logouthandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false)
+    };
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
 
 export default AuthContext;
