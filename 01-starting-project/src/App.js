cf913c45cbe3baa18cfdce2fb427adf69f34c0e7
Ritesh jangra <rjprince18@gmail.com>
2023-04-14 00:29:54 +0530

    Meals App

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,11 +1,12 @@
+import { Fragment } from 'react';
 import Header from './components/Layout/Header'
 
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-    </div>
+    </Fragment>
   );
 }
 
---
+++ 01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = (props) => {
+    return (
+        <>
+            <button>
+                
+            </button>
+        </>
+    )
+}
+
+export default HeaderCartButton;
